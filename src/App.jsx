972a3a851d679cf3c4a576f3d38fda1aec4e2fc3
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-04-04 12:47:23 -0400

    feat: add bg, loading and error  images

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Bookshelf from "@/components/BookShelf"
 
 export default function App() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-[#0A0A0A] to-[#1A1A1A] p-4 md:p-8">
+    <main className="min-h-screen bg-gradient-to-b from-[#0A0A0A] to-[#1A1A1A] p-4 md:p-8 bg-[url(/public/background.webp)] bg-cover">
       <div className="max-w-7xl mx-auto">
         <header className="text-center mb-12 pt-8">
           <h1 className="text-4xl md:text-5xl font-serif text-[#D4AF37]/80 mb-2">
