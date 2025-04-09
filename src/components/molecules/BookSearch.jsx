12c385e14793605d00f02cdbbbe125ca894bf1ec
JesusMiguel03 <jmdefreitas10@gmail.com>
2025-04-09 19:02:31 -0400

    refactor(styles): update BookSearch

--- src/components/molecules/BookSearch.jsx
+++ src/components/molecules/BookSearch.jsx
@@ -13,7 +13,7 @@ export default function BookSearch({ setSearch }) {
       <h2 className="text-2xl font-bold mb-4">Search a book</h2>
       <input
         type="text"
-        className="w-full p-2 border border-gray-300 rounded-md"
+        className="w-full py-2.5 rounded-xl px-5 bg-white/10 focus:bg-white/30 outline-0 border-none delay-75 transition-all duration-300 placeholder:text-white/50 hover:bg-white/30"
         placeholder="Search by title or author..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
