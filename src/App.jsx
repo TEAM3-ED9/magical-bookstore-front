32815dedf3e6379625c238b03da0e4fb5b4a6d29
yansel17 <yanselj6@gmail.com>
2025-04-09 23:18:11 -0400

    Arreglo del scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Bookshelf from "@/components/BookShelf"
+import Bookshelf from "@/components/BookShelf";
 
 export default function App() {
   return (
-    <main className="min-h-screen bg-gradient-to-b from-[#0A0A0A] to-[#1A1A1A] p-4 md:p-8 bg-[url(/public/background.webp)] bg-cover text-white">
-      <div className="fixed w-full inset-0 bg-black/30 backdrop-blur-sm z-50">
-        <div className="max-w-7xl mx-auto">
+    <main className="min-h-screen bg-gradient-to-b from-[#0A0A0A] to-[#1A1A1A] bg-[url(/public/background.webp)] bg-cover text-white h-screen overflow-x-hidden">
+      <div className=" w-full h-full inset-0 bg-black/30 backdrop-blur-sm z-50">
+        <div className="max-w-7xl h-full mx-auto">
           <header className="p-6 sm:p-0 sm:pt-8 text-center sm:mb-6">
             <h1 className="text-4xl md:text-5xl font-serif text-title sm:text-title/80 mb-2">
               The Atrio of Rowena
@@ -17,5 +17,5 @@ export default function App() {
         </div>
       </div>
     </main>
-  )
+  );
 }
